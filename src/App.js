3ac52ec0181bb36c6555eca8a7a706ae05a9ffbc
yab <yab>
2021-06-26 17:28:15 +0530

    desktop design

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
 import './App.css';
-import desktopImage from "./image-header-desktop.jpg";
+// import desktopImage from "./image-header-desktop.jpg";
 
 function App() {
   return (
     <div className="container">
         <div className="left">
-      <div className="columns">
-          <h1 className="title">
+          <div className="columns">
+              <h1 className="title">
+                Get <span className="stat-header">insights</span> that help your business grow.
+              </h1>
+              <p className="stat"> 
+                Discover the benefits of data analytics and make better decisions regarding revenue, customer 
+                experience, and overall efficiency.
+              </p>
 
-          Get <span className="stat-header">insights</span> that help your business grow.
-          </h1>
-          <p className="stat"> 
-          Discover the benefits of data analytics and make better decisions regarding revenue, customer 
-          experience, and overall efficiency.
-          </p>
+              <div className="stat-row">
+                <div><h2>10k+</h2> <h3>companies</h3></div>
+                <div><h2 className="">314</h2> <h3>templates</h3></div>
+                <div><h2>12M+</h2> <h3>queries</h3></div>
+              </div>
 
-          <div className="stat-row">
-            <div> 10k+ companies</div>
-            <div>314 templates</div>
-            <div>12m+ queries</div>
-          </div>
-
-          <div class="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-            Coded by <a href="https://github.com/yabets">Yabets</a>.
-          </div>
+              <div class="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
+                Coded by <a href="https://github.com/yabets">Yabets</a>.
+              </div>
+            </div>
         </div>
-      </div>
         <div className="right">
-          <img className="image" src={desktopImage} alt="team working in group" />
+          {/* <img className="image" src={desktopImage} alt="team working in group" /> */}
         </div>
       
       
